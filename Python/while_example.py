4f6343e52b03089682dcdbe8223002d23d05bc57
7tena <athenrenjit@gmail.com>
2026-01-11 22:21:03 +0530

    While, For, Range in Python

--- Python/while_example.py
+++ Python/while_example.py
@@ -1,4 +1,25 @@
-spam=0
-while spam < 5:
-    print('Hello world')
-    spam = spam+1
+# spam=0
+# while spam < 5:
+#     print('Hello world')
+#     spam = spam+1
+
+# name = ''
+# while name != 'your name':
+#     print('Please type your name.')
+#     name = input()
+# print('Thank you!')
+
+# name = 'Jimmy'
+# for i in range(5):
+#     print('Jimmy five times (' + str(i) + ')')
+
+# total = 0
+# for num in range(101):
+#     total = total + num
+# print(total)
+
+# range(start, stop, step)
+for i in range(0, 10, 2):
+    print(i)
+for i in range(5, 0, -1):
+    print(i)
