0db07f7447c1b7a5b0d3ab6df608116806670d48
7tena <athenrenjit@gmail.com>
2026-01-12 22:58:39 +0530

    built in function.

--- Python/basics.py
+++ Python/basics.py
@@ -19,7 +19,20 @@
 # print(total)
 
 # range(start, stop, step)
-for i in range(0, 10, 2):
-    print(i)
-for i in range(5, 0, -1):
-    print(i)
+# for i in range(0, 10, 2):
+#     print(i)
+# for i in range(5, 0, -1):
+#     print(i)
+
+# import random
+# i = random.randint(1, 10)
+# print(i)
+
+from random import *
+i = randint(1, 10)
+print(i)
+
+import sys
+print('Hello')
+sys.exit()
+print('Goodbye')  # This line will not be executed
